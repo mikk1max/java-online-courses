8437a625ae99a102de9522f84e2170b17d6f9e24
PusyHunter <98088572+PusyHunter@users.noreply.github.com>
2024-12-15 20:02:52 +0100

    feat: add client application, add Vigenere Cipher encryption and descryption in Client class and FileController class

--- src/main/java/com/example/onlinecourses/controller/FileController.java
+++ src/main/java/com/example/onlinecourses/controller/FileController.java
@@ -1,5 +1,6 @@
 package com.example.onlinecourses.controller;
 
+import com.example.onlinecourses.util.VigenereCipher;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.*;
@@ -9,24 +10,23 @@ import java.io.IOException;
 import java.nio.file.*;
 import java.util.List;
 import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping("/api/files")
 public class FileController {
-
     private final String dataDir = "data";
 
 
     @GetMapping
     public ResponseEntity<List<String>> listFiles() {
         try {
-            // Получаем путь к папке
             ClassPathResource resource = new ClassPathResource(dataDir);
             Path folderPath = resource.getFile().toPath();
 
-            // Список файлов
             List<String> files = Files.list(folderPath)
-                                      .map(path -> path.getFileName().toString())
-                                      .collect(Collectors.toList());
+                    .map(path -> path.getFileName().toString())
+                    .filter(fileName -> !fileName.equals(".gitkeep"))
+                    .collect(Collectors.toList());
             return ResponseEntity.ok(files);
         } catch (IOException e) {
             e.printStackTrace();
@@ -38,15 +38,16 @@ public class FileController {
     @GetMapping("/{filename}")
     public ResponseEntity<String> getFile(@PathVariable String filename) {
         try {
-
-            ClassPathResource resource = new ClassPathResource(dataDir + "/" + filename);
+            String encryptionKey = "MYSECRETKEY";
+            String decryptedFilename = VigenereCipher.decrypt(filename, encryptionKey);
+            ClassPathResource resource = new ClassPathResource(dataDir + "/" + decryptedFilename);
             if (!resource.exists()) {
                 return ResponseEntity.notFound().build();
             }
-
-
             String content = StreamUtils.copyToString(resource.getInputStream(), java.nio.charset.StandardCharsets.UTF_8);
-            return ResponseEntity.ok(content);
+
+            String encryptedContent = VigenereCipher.encrypt(content, encryptionKey);
+            return ResponseEntity.ok(encryptedContent);
         } catch (IOException e) {
             e.printStackTrace();
             e.printStackTrace();
